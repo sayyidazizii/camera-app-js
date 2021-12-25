992529221e18276642a9f0d7e7f4ed65f910cd5c
sayyid <sayyidsyafiq234@gmail.com>
2021-12-25 07:51:39 +0700

    commit pertama

---
+++ app.js
@@ -0,0 +1,34 @@
+// Set constraints for the video stream
+//batasan vidio pada kamera tanpa suara
+var constraints = { video: { facingMode: "user" }, audio: false };
+// Define constants
+//definisikan variabelnya
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+//permintaan akses kamera perangkat 
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
+// Mengambil gambar Ketika cameraTriger di tap
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// Start the video stream when the window loads
+//memulai vidio langsung ketika loading page
+window.addEventListener("load", cameraStart, false);
